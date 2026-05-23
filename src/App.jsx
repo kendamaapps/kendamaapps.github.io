410f2ad97b890375766f3d1845cbb16f828b2a67
Harrison Li <hli76@student.ubc.ca>
2026-05-23 12:24:22 -0700

    add home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { useState } from 'react';
 import Navbar from './components/Navbar.jsx';
 import Galaxy from './components/Galaxy.jsx';
+
+import Home from './tabs/Home.jsx';
 import Generator from './tabs/Generator.jsx';
 import Timer from './tabs/Timer.jsx';
 import Tracker from './tabs/Tracker.jsx';
 
 const TABS = {
+  home: Home,
   generator: Generator,
   timer:     Timer,
   tracker:   Tracker,
 };
 
 export default function App() {
-  const [activeTab, setActiveTab] = useState('generator');
+  const [activeTab, setActiveTab] = useState('home');
   const TabComponent = TABS[activeTab];
 
   return (
