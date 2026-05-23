53315b33d8a41a95d9b482bf06fd8f7be7011467
Harrison Li <hli76@student.ubc.ca>
2026-05-23 12:14:11 -0700

    update galaxy

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ export default function App() {
 
   return (
     <>
-      <div id="galaxy-bg" style={{ width: '100%', height: '600px', position: 'relative' }}>
+      <div id="galaxy-bg">
         <Galaxy 
           mouseRepulsion
           mouseInteraction
